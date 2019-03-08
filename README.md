7b8789e816131c97f4dc95c9dfb4150fdfafbb69
alzamora <soyborisalzamora@gmail.com>
2019-03-08 15:05:07 -0500

    Initial configuration of flask container for deployment

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mldeployment
 Simple Flask Machine Learning Deployment
+
+The following project assumes to have a previously trained model (in this case a SVM classifier) and a specific use case for a REST API which will define de set of parameters analyzed, in this case will be the ones realted to the iris dataset.
---
+++ app.py
@@ -0,0 +1,30 @@
+import pickle
+from flask import request
+from flask import Flask
+app = Flask(__name__)
+
+# Trained iris classifier using svm
+filename = 'model.pkl' 
+
+@app.route("/")
+def main():
+	return "<h3>1, 2, 1 , 2 ... is this thing on?</h3>"
+ 
+@app.route("/predict/", methods=['GET'])
+def predict():
+	# ['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)']
+	# Example:  [6.9, 3.1, 4.9, 1.5]
+	sepal_length = request.args.get('sepal_length')
+	sepal_width = request.args.get('sepal_width')
+	petal_length = request.args.get('petal_length')
+	petal_width = request.args.get('petal_width')
+	feature_set = [sepal_length, sepal_width, petal_length, petal_width]
+	target_names = ['setosa', 'versicolor', 'virginica']
+	# load the model from disk
+	model = pickle.load(open(filename, 'rb'))
+	result = model.predict([feature_set])
+	
+	return target_names[int(result)]
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=5000, debug=True)
